fe1fc14e1c2a3b06c2f51e6c84632bf14178f36a
Programming Hero <programming.hero1@gmail.com>
2021-11-04 02:25:24 -0500

    api can filter by date

--- index.js
+++ index.js
@@ -19,11 +19,23 @@ async function run() {
         const database = client.db('doctors_portal');
         const appointmentsCollection = database.collection('appointments');
 
+        app.get('/appointments', async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+
+            const query = { email: email, date: date }
+
+            const cursor = appointmentsCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+
         app.post('/appointments', async (req, res) => {
             const appointment = req.body;
             const result = await appointmentsCollection.insertOne(appointment);
-            res.json(result);
-        })
+            console.log(result);
+            res.json(result)
+        });
 
     }
     finally {
