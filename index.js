c703959cbe23ee911407e018f03c98a73027d5bb
Programming Hero <programming.hero1@gmail.com>
2021-11-04 22:16:55 -0500

    added admin route

--- index.js
+++ index.js
@@ -46,11 +46,19 @@ async function run() {
 
         app.put('/users', async (req, res) => {
             const user = req.body;
-            console.log('put', user);
             const filter = { email: user.email };
             const options = { upsert: true };
             const updateDoc = { $set: user };
             const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log('put', user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
             res.json(result);
         })
 
