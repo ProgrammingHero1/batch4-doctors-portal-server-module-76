e823dd7123b9b1ca0bf0f7d3677c395204adc302
Programming Hero <programming.hero1@gmail.com>
2021-11-05 23:38:05 -0500

    removed locale string for date

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function run() {
 
         app.get('/appointments', verifyToken, async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
 
             const query = { email: email, date: date }
 
