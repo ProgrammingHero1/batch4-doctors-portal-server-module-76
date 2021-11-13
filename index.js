309c1690c8a716f7bf1055dfc7c1e50d0202054a
Programming Hero <programming.hero1@gmail.com>
2021-11-13 16:14:16 -0600

    api to load appointment specific data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const admin = require("firebase-admin");
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 const port = process.env.PORT || 5000;
 
@@ -52,6 +53,13 @@ async function run() {
             const cursor = appointmentsCollection.find(query);
             const appointments = await cursor.toArray();
             res.json(appointments);
+        })
+
+        app.get('/appointments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await appointmentsCollection.findOne(query);
+            res.json(result);
         })
 
         app.post('/appointments', async (req, res) => {
